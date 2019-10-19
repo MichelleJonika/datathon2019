d7af9d14baf9fed76ded6fbaf87544f71858b658
Nathan <nathan.wally.anderson@gmail.com>
2019-10-19 14:30:59 -0500

    bigsupervector

--- tacosandburritos.R
+++ tacosandburritos.R
@@ -2,7 +2,7 @@ data <- read.csv('just tacos and burritos.csv')
 x <- head(data)
 x
 data <- data[,1:26]
-data <- cbind(data, 'beef')
+# data <- cbind(data, 'beef')
 
 beefcol <- vector(length = nrow(data))
 for(i in 1:nrow(data)){
@@ -16,13 +16,17 @@ colnames(beefcol) <- c('Beef')
 
 all.desc <- ''
 for(i in 1:nrow(data)){
-  all.desc <- paste(all.desc, as.character(data$menus.description[i]))
+  all.desc <- paste(all.desc, as.character(data$menus.description[i]), sep = ',')
 }
 
 diff.ing <- strsplit(all.desc, split = ',')
+diff.ing <- diff.ing[[1]]
+diff.ing <- unique(diff.ing)
+
+
 
 tacovec <- vector(length = nrow(data))
-vurritovec <- vector(length = nrow(data))
+burritovec <- vector(length = nrow(data))
 for(i in 1:nrow(data)){
   if(grepl('taco', tolower(as.character(data$menus.name[i])))){
     tacovec[i] <- 1
