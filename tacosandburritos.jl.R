b8c26f7dd0a019bccfade36ffaaa15067f798d2b
johnathanlo <49450170+johnathanlo@users.noreply.github.com>
2019-10-19 22:28:45 -0500

    sdsdsd

--- tacosandburritos.jl.R
+++ tacosandburritos.jl.R
@@ -90,6 +90,8 @@ tacos.trimmed.merge[tacos.trimmed.merge$cheddar, "cheese"]= TRUE
 tacos.trimmed.merge[tacos.trimmed.merge$american, "cheese"] = TRUE
 tacos.trimmed.merge[tacos.trimmed.merge$fresco, "cheese"] = TRUE
 tacos.trimmed.merge[tacos.trimmed.merge$cheesesteak, "cheese"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$feta, "cheese"] = TRUE
+
 
 tacos.trimmed.merge[tacos.trimmed.merge$steak, "beef"] = TRUE
 tacos.trimmed.merge[tacos.trimmed.merge$shortrib, "beef"] = TRUE
@@ -101,11 +103,14 @@ tacos.trimmed.merge[tacos.trimmed.merge$kobe, "beef"] = TRUE
 tacos.trimmed.merge[tacos.trimmed.merge$brisket, "beef"] = TRUE
 tacos.trimmed.merge[tacos.trimmed.merge$ground.beef, "beef"] = TRUE
 tacos.trimmed.merge[tacos.trimmed.merge$hamburger, "beef"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$kobe, "beef"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$pastrami, "beef"] = TRUE
 
 tacos.trimmed.merge[tacos.trimmed.merge$refried, "bean"] = TRUE
 tacos.trimmed.merge[tacos.trimmed.merge$black.bean, "bean"] = TRUE
 tacos.trimmed.merge[tacos.trimmed.merge$lentil, "bean"] = TRUE
 tacos.trimmed.merge[tacos.trimmed.merge$pinto, "bean"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$colorado, "bean"] = TRUE
 
 tacos.trimmed.merge[tacos.trimmed.merge$wahoo, "fish"] = TRUE
 tacos.trimmed.merge[tacos.trimmed.merge$mahi, "fish"] = TRUE
@@ -127,8 +132,109 @@ tacos.trimmed.merge[tacos.trimmed.merge$grouper, "fish"] = TRUE
 tacos.trimmed.merge[tacos.trimmed.merge$eel, "fish"] = TRUE
 tacos.trimmed.merge[tacos.trimmed.merge$swai, "fish"] = TRUE
 
- 
-ifelse(tacos.trimmed.merge$jack, tacos.trimmed.merge$cheese = T)
+tacos.trimmed.merge[tacos.trimmed.merge$ham, "pork"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$prosciutto, "pork"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$carnita, "pork"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$bacon, "pork"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$pastor, "pork"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$pulled.pork, "pork"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$machaca, "pork"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$belly, "pork"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$sausage, "pork"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$adobada, "pork"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$chorizo, "pork"] = TRUE
+
+tacos.trimmed.merge[tacos.trimmed.merge$sauce, "sauce"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$tomatillo, "sauce"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$chihuahua, "sauce"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$ranch, "sauce"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$guac, "sauce"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$sour.cream, "sauce"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$sriracha, "sauce"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$crema, "sauce"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$red, "sauce"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$roja, "sauce"]= TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$rojo, "sauce"]=TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$pico, "sauce"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$verde, "sauce"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$choco, "sauce"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$wasabi, "sauce"]=TRUE
+
+
+tacos.trimmed.merge[tacos.trimmed.merge$fish, "seafood"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$calamari, "seafood"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$octopus, "seafood"]=TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$prawn, "seafood"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$shrimp, "seafood"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$crab, "seafood"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$lobster, "seafood"] = TRUE
+
+tacos.trimmed.merge$onion <- NULL
+tacos.trimmed.merge$tomato <- NULL
+tacos.trimmed.merge$lettuce <- NULL
+tacos.trimmed.merge$kale.1 <- NULL
+tacos.trimmed.merge$specialtyveg <- c()
+
+tacos.trimmed.merge[tacos.trimmed.merge$avocado, "specialtyveg"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$olive, "specialtyveg"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$yuca, "specialtyveg"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$yucca, "specialtyveg"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$squash, "specialtyveg"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$agave, "specialtyveg"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$relleno, "specialtyveg"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$sweet.potato, "specialtyveg"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$spinach, "specialtyveg"]=TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$yam, "specialtyveg"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$agave, "specialtyveg"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$peanut, "specialtyveg"]= TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$portobello, "specialtyveg"]=TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$almond, "specialtyveg"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$kale, "specialtyveg"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$cactus, "specialtyveg"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$jicama, "specialtyveg"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$carrot, "specialtyveg"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$cucumber, "specialtyveg"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$kimchi, "specialtyveg" ] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$chipotle, "specialtyveg"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$pacilla, "specialtyveg"] = TRUE
+tacos.trimmed.merge[is.na(tacos.trimmed.merge$specialtyveg), "specialtyveg"] = FALSE
+
+tacos.trimmed.merge[tacos.trimmed.merge$tofu, "vegetarian"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$seitan, "vegetarian"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$tempeh, "vegetarian"] = TRUE
+
+tacos.trimmed.merge[tacos.trimmed.merge$pulled.pork, "carnita"] = TRUE
+tacos.trimmed.merge$pulled.pork <- NULL                    
+
+tacos.trimmed.merge[tacos.trimmed.merge$prawn, "shrimp"] = TRUE
+tacos.trimmed.merge$prawn<- NULL
+tacos.trimmed.merge[tacos.trimmed.merge$prime.rib, "primerib"] = TRUE
+tacos.trimmed.merge$prime.rib <- NULL
+tacos.trimmed.merge[tacos.trimmed.merge$short.rib, "shortrib"] = TRUE
+tacos.trimmed.merge$short.rib <- NULL
+tacos.trimmed.merge[tacos.trimmed.merge$maiz, "corn"] = TRUE
+tacos.trimmed.merge$maiz <- NULL
+tacos.trimmed.merge[tacos.trimmed.merge$roja, "red"] = TRUE
+tacos.trimmed.merge[tacos.trimmed.merge$rojo, "red"]= TRUE
+tacos.trimmed.merge$roja <- NULL
+tacos.trimmed.merge$rojo<- NULL
+tacos.trimmed.merge[tacos.trimmed.merge$ground.beef, "hamburger"] = TRUE
+tacos.trimmed.merge$ground.beef <- NULL
+tacos.trimmed.merge[tacos.trimmed.merge$sausage, "chorizo"] = TRUE
+tacos.trimmed.merge$sausage <- NULL
+tacos.trimmed.merge[tacos.trimmed.merge$sweet.potato, "yam"] = TRUE
+tacos.trimmed.merge$sweet.potato <- NULL
+tacos.trimmed.merge[tacos.trimmed.merge$green.chile, "pepper"] = TRUE
+tacos.trimmed.merge$green.chile <- NULL
+tacos.trimmed.merge[tacos.trimmed.merge$pescado, "fish"] = TRUE
+tacos.trimmed.merge$pescado <- NULL
+tacos.trimmed.merge[tacos.trimmed.merge$yuca, "yucca"] = TRUE
+tacos.trimmed.merge$yuca <- NULL
+tacos.trimmed.merge[tacos.trimmed.merge$dorrados, "dorados"] = TRUE
+tacos.trimmed.merge$dorrados<- NULL
+tacos.trimmed.merge[tacos.trimmed.merge$portobello, "mushroom"] = TRUE
+tacos.trimmed.merge$portobello <- NULL
+
 ## family
 meatvec <- vector(length = nrow(data)) # beef, chicken, pork
 cheesevec <- vector(length = nrow(data)) # cheddar, american, jack, colby, fresco
